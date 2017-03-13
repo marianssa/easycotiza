59b1e86cd6fe3de2b41acd26400f54bcd53590c6
marianssa <mariajoselaylay@gmail.com>
2017-03-13 02:02:53 -0300

    heroku

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 
 module Proyecto
   class Application < Rails::Application
-    config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
     config.action_mailer.default_url_options = { :host => 'localhost:3000' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
