4947fdce2622504ce1a004d8d05372a75b8508da
diegomaui <diego.pavez.cortez@gmail.com>
2017-03-15 21:42:34 -0300

    agregar planes a admin

---
+++ app/admin/plane.rb
@@ -0,0 +1,10 @@
+ActiveAdmin.register Plane do
+index do
+		selectable_column
+    	id_column
+    	column "Nombre Plan", :nombre
+    	column "Rut", :precio
+    	column "Rut", :meses	
+    	column "Rut", :descripcion
+end
+end
