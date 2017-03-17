995171f620e04b8d251e898c6fbe3600a492a3a5
diegomaui <diego.pavez.cortez@gmail.com>
2017-03-17 04:58:41 -0300

    detalle 3

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,8 @@ class Answer < ApplicationRecord
 	belongs_to :enterprise
 	belongs_to :cotizacion
 	validates :answer_cotiza, presence: true,length: { maximum: 200 }
+	validates :enterprise_id, uniqueness: { scope: :cotizacion_id, message: "¡Ya has entregado tu respuesta!" }
+
+    belongs_to :cotizacion
+    belongs_to :enterprise
 end
