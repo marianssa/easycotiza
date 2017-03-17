47ad668514184ce7c0ec8f2a6c7bb09044159862
marianssa <mariajoselaylay@gmail.com>
2017-03-17 01:54:32 -0300

    pagos

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -5,19 +5,22 @@ ActiveAdmin.register Payment do
 	scope :No_pagados do |pagos|
 		pagos.where(:canceled => false)
 	end
-	#index do
- 	#	selectable_column
-    #	id_column
-    #	column "Nombre_Empresa", :empresa_nombre
-    #	column "tiempo_plan", :tiempo_termino
-    #	image_tag("products/" + Payment.imagen)
-   # end
+	index do
+ 	selectable_column
+    id_column
+    column "Nombre_Empresa", :empresa_nombre
+    column "tiempo_plan", :tiempo_termino
+    column('Recibida en', :created_at)
+   end
 
-    index :as => :grid do |pagos|
-    div do
-      a :href => admin_payment_path(pagos) do
-        image_tag(pagos.imagen)
-      end
-end
+
+show do
+  attributes_table do
+    row :id
+    row :canceled
+     row :imagen do |ad|
+        image_tag ad.imagen.url
+ end
+  end
 end
 end
